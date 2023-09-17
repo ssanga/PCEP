83b2c41ecfceb923c30ea1461b520a9cf5347047
Santiago Sanchez Garcia <ssanga@outlook.com>
2023-09-17 13:08:57 +0200

    pause

---
+++ src/03_operators-and-bindings.py
@@ -0,0 +1,10 @@
+# unary and bitwise operators
+a = 0b010
+print(a)
+print(bin(a))
+# ALT + 126
+print(~a)
+# mismo resultado que la linea anterior con la tilde de la ñ
+print(-a - 1)
+
+print(bin(~a))
