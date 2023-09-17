a33cf30712a7d4db7571be6110b1b7519d0ccfed
Santiago Sanchez Garcia <ssanga@outlook.com>
2023-09-17 13:02:52 +0200

    02_number-systems-and-numeric-operators

---
+++ src/02_number-systems-and-numeric-operators.py
@@ -0,0 +1,14 @@
+print (2 + 2)
+print (2.0 + 2.0)
+print (2.0 + 2)
+print (3 * 9)
+print (5 / 3)
+print (6 / 3)
+print (5 // 3)
+print (6 // 3)
+# remainder
+print (5 % 3)
+print (12 % 2)
+print (13 % 2)
+# power
+print (2 ** 3)
