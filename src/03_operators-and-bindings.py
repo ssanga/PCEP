a3a0ff662718cfc0f59cd72f783cdd390e85d7d0
ssanga <ssanga@cecabank.es>
2023-09-18 17:18:12 +0200

    queda por ver print function

--- src/03_operators-and-bindings.py
+++ src/03_operators-and-bindings.py
@@ -9,4 +9,60 @@ print(-a - 1)
 
 print(bin(~a))
 
-# prueba desde el pc virtual
+a = 0b1001
+b = 0b1100
+print(bin(a | b )) #OR
+print(bin(a & b )) #AND
+print(bin(a ^ b )) #XOR
+
+a = 0b110
+print(bin(a >> 2))
+print(bin(a >> 4))
+print(bin(a << 2))
+
+# Boolean Operators
+print(not True)
+print(not False)
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+
+# Comparision Operators
+print(1 < 2)
+print(2 > 1)
+print(2 < 1)
+print(1 <= 1)
+print(2.0 >= 3)
+print(2.0 >= 2)
+print('a' > 'b')
+print('b' > 'a') #True
+print('bb' >= 'ba') #True
+print('a' <= 'c') #True
+print(ord('a'))
+print(ord('b'))
+print(ord('c'))
+
+# esto no se puede
+# print('a' <= 1)
+
+print(1 == 1)
+print('a' == 1)
+print(1 != 2)
+
+print(1 is 1)
+print(1 is 1.0) #False
+print('a' is 'a')
+print('a' is not 'a')
+print(id('a'))
+print([] is []) #False
+
+# Operator priority
+
+
+
---
+++ src/04_input-and-output-operations.py
@@ -0,0 +1,16 @@
+# typecasting
+print(float(1))
+print(int(1.3)) #truncate
+print(str(1.0))
+print(str(False))
+print(float('1.2'))
+print(bool(1))
+print(bool(0))
+print(bool(0.0))
+print(bool('Pepe'))
+print(bool(''))
+
+# input
+name = input("What is you name? ")
+favorite = input("Favorite Color: ")
+age = int(input("How old are you? "))
