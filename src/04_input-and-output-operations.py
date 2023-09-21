dd22069a12de43d94150f169d111d004e1ace736
Santiago Sanchez Garcia <ssanga@outlook.com>
2023-09-21 09:00:03 +0200

    fin modulo

--- src/04_input-and-output-operations.py
+++ src/04_input-and-output-operations.py
@@ -12,5 +12,16 @@ print(bool(''))
 
 # input
 name = input("What is you name? ")
-favorite = input("Favorite Color: ")
+color = input("Favorite Color: ")
 age = int(input("How old are you? "))
+
+# print
+print(name)
+print("is " + str(age) + " years old")
+print("and loves the color " + color + ".")
+
+print(name, end=" ")
+print("is " + str(age) + " years old", end=" ")
+print("and loves the color " + color + ".", end=" ")
+
+print(name, 'is', age, 'years old and loves the color', color + '.')
