48e9514cc0a3687485b726d383196ffc248578c8
Santiago Sanchez Garcia <ssanga@outlook.com>
2023-09-21 09:05:51 +0200

    hands on lab 1

---
+++ src/05_to-celsius.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input("What temperature (in Fahrenheit) would you like to convert converted to Celsius? "))
+celsius = (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit, "F is", round(celsius,2), "C")
+
