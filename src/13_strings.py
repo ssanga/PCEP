dad41ad360f343e8435cf7061eecea702d4720da
Santiago Sanchez Garcia <ssanga@outlook.com>
2023-09-24 20:19:20 +0200

    me he quedado en useful string methods, part 1, a medias

---
+++ src/13_strings.py
@@ -0,0 +1,10 @@
+print(ord('a'))
+print('\u2122')
+print(chr(8482))
+
+# useful String methods
+my_str = "This is an example"
+print(my_str.lower())
+print(my_str.upper())
+print(my_str.capitalize())
+print(my_str.title())
